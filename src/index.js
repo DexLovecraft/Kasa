876f2ea398db7aa5c7b1e9c05ec06e8f734279d9
Alex Balmes <alex.balmes1912@gmail.com>
2023-06-20 17:34:21 +0200

    Banner component for both home and about

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css'
 
 import Header from './components/header';
+import Banner from './components/banner';
+
+import home__banner from './img/home__banner.png'
+import about__banner from './img/about__banner.png'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.Fragment>
     <Header />
+    <Banner img={home__banner} text={'Chez vous, partout et ailleurs'}/>
+    <Banner img={about__banner} text={''}/>
   </React.Fragment>
 );
