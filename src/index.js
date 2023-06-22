7289bc1aa6e4d3e23ca574a056daa389f854a513
Alex Balmes <alex.balmes1912@gmail.com>
2023-06-22 12:05:40 +0200

    preparation for push

--- src/index.js
+++ src/index.js
@@ -3,23 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css'
 
 import Header from './components/header';
-// import Banner from './components/banner';
-// import Gallery from './components/gallery';
+import Banner from './components/banner';
+import Gallery from './components/gallery';
 import Footer from './components/footer';
-import Error404 from './components/error404';
+// import Error404 from './components/error404';
 
-// import home__banner from './img/home__banner.png'
+import home__banner from './img/home__banner.png'
 // import about__banner from './img/about__banner.png'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.Fragment>
     <Header />
-    {/* <Banner img={home__banner} text={'Chez vous, partout et ailleurs'}/>
-    <Banner img={about__banner} text={''}/> Banner pour la page a propos
-    <Thumbnail img={home__banner} name="Maison des lilas" id="021522"/>
-    <Gallery /> */}
-    <Error404 />
+    <Banner img={home__banner} text={'Chez vous, partout et ailleurs'}/>
+    {/* <Banner img={about__banner} text={''}/> Banner pour la page a propos */}
+    <Gallery />
+    {/* <Error404 /> */}
     <Footer/>
   </React.Fragment>
 );
