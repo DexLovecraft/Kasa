7df471b2054f79cd1c784beb74394a6547efe9ea
Alex Balmes <alex.balmes1912@gmail.com>
2023-06-23 15:38:00 +0200

    house container components

---
+++ src/components/House/houseContainer.jsx
@@ -0,0 +1,11 @@
+import '../../styles/House/houseContainer.css'
+
+function House () {
+    return (
+        <div className="house__container">
+            <p>Test</p>
+        </div>
+    )
+}
+
+export default House
--- src/index.js
+++ src/index.js
@@ -3,23 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css'
 
 import Header from './components/Shared/header';
-import Banner from './components/Shared/banner';
-// import Gallery from './components/Home/gallery';
 import Footer from './components/Shared/footer';
-import CollapseAbout from './components/About/collapseAbout';
+// import Banner from './components/Shared/banner';
+// import Gallery from './components/Home/gallery';
+// import CollapseAbout from './components/About/collapseAbout';
+import House from './components/House/houseContainer';
 // import Error404 from './components/error404';
 
 // import home__banner from './img/home__banner.png'
-import about__banner from './img/about__banner.png'
+// import about__banner from './img/about__banner.png'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.Fragment>
     <Header />
-    {/* <Banner img={home__banner} text={'Chez vous, partout et ailleurs'}/> */}
-    <Banner img={about__banner} text={''}/>
-    <CollapseAbout/>
-    {/* <Gallery /> */}
+    {/* <Banner img={home__banner} text={'Chez vous, partout et ailleurs'}/>
+    <Gallery /> */}
+    {/* <Banner img={about__banner} text={''}/>
+    <CollapseAbout/> */}
+    <House/>
     {/* <Error404 /> */}
     <Footer/>
   </React.Fragment>
