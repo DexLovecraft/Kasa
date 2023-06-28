26bdb5f23a67f53cd2ca80dbaa95e1592c363b76
Alex Balmes <alex.balmes1912@gmail.com>
2023-06-28 17:01:04 +0200

    delete space

--- src/components/Shared/header.jsx
+++ src/components/Shared/header.jsx
@@ -38,40 +38,6 @@ function Header (props) {
                     </div>
                 )
         }
-
-        // if (selection === 'accueil'){
-        //     return (
-        //         <div className="header">
-        //             <img src={logo__global} alt='Logo of Kasa' className='header__logo'/>
-        //             <ul className='header__menu'>
-        //                 <Link to='/' className='header__menu__button selected link'>Accueil</Link>
-        //                 <Link to='/about'className='header__menu__button link' onClick={() => {changeSelection('about')}}>A propos</Link>
-        //             </ul>
-        //         </div>
-        //     )
-        // }
-        // else if (selection === "about") {
-        //     return (
-        //         <div className="header">
-        //             <img src={logo__global} alt='Logo of Kasa' className='header__logo'/>
-        //             <ul className='header__menu'>
-        //                 <Link to='/' className='header__menu__button link' onClick={() => {changeSelection('accueil')}}>Accueil</Link>
-        //                 <Link to='/about'className='header__menu__button selected link'>A propos</Link>
-        //             </ul>
-        //         </div>
-        //     )
-        // } 
-        // else {
-        //     return (
-        //         <div className="header">
-        //             <img src={logo__global} alt='Logo of Kasa' className='header__logo'/>
-        //             <ul className='header__menu'>
-        //                 <Link to='/' className='header__menu__button link' onClick={() => {changeSelection('accueil')}}>Accueil</Link>
-        //                 <Link to='/about'className='header__menu__button link' onClick={() => {changeSelection('about')}}>A propos</Link>
-        //             </ul>
-        //         </div>
-        //     )
-        // }   
 }
 
 export default Header
